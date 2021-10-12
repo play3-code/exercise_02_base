1ca4d08f3bb84392f919d5c5eca3409761fb311c
chrischne <chrischne@gmail.com>
2021-10-12 14:32:02 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,4 @@
 let geodata;
-let streets;
 
 let bounds = {
   left: 8.20782,
@@ -15,38 +14,13 @@ function preload() {
 function setup() {
   createCanvas(900, 650);
 
+  console.log(geodata);
+
   noLoop();
 }
 
 function draw() {
   background(255);
-
-  for (let i = 0; i < 10; i++) {
-    let waterObject = waterData[i];
-
-    let geometry = waterObject.geometry;
-    let properties = waterObject.properties;
-    let name = properties.name;
-    console.log("name", name);
-    let coordinates = geometry.coordinates[0][0];
-
-    noStroke();
-    fill(random(0, 255), random(0, 255), random(255, 0));
-    beginShape();
-    // console.log("coordinates.length", coordinates.length);
-    for (let j = 0; j < coordinates.length; j++) {
-      let coord = coordinates[j];
-      let lat = coord[1];
-      let lon = coord[0];
-
-      let x = map(lon, bounds.left, bounds.right, 0, width);
-      let y = map(lat, bounds.top, bounds.bottom, 0, height);
-      // console.log(x, y);
-
-      curveVertex(x, y);
-    }
-    endShape();
-  }
 }
 
 function keyTyped() {
